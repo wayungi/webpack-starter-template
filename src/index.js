e0817c2ae5cf87f99ef7228ea1d55fc7785f28c6
wayungi <fwayungi@gmail.com>
2022-08-03 01:42:20 +0300

    Add dev-server scripts

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
+/** @format */
+
 import './style.css';
+import Icon from './review.png';
 
 function component() {
   const element = document.createElement('div');
+  element.classList.add('hello');
 
-  // Lodash, currently included via a script, is required for this line to work
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  // Add the image to our existing div.
+
+  const myIcon = new Image();
+
+  myIcon.src = Icon;
+
+  element.appendChild(myIcon);
 
   return element;
 }
--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,13 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  mode: 'development',
+
   entry: './src/index.js',
+
+  devServer: {
+    static: './dist',
+  },
 
   plugins: [
     new HtmlWebpackPlugin({
@@ -16,6 +22,10 @@ module.exports = {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
+  },
+
+  optimization: {
+    runtimeChunk: 'single',
   },
 
   module: {
